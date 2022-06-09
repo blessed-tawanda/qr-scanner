072a89ebc9fe478ad6e8991e9f48a2a39dafff8c
blessed-tawanda <blessedmahuni@gmail.com>
2022-06-09 12:23:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { useEffect } from "react";
 
 function App() {
+  const onScanSuccess = (decodedText, decodedResult) => {
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    alert(JSON.stringify(decodedResult))
+  }
+
+  const onScanFailure = (error) => {
+    console.error(`Code Scan Error ${error}`);
+  }
+
+  useEffect(() => {
+    const html5QrcodeScanner = new Html5QrcodeScanner("scanner", {fps: 10, qrbox: {width: 250, height: 250}, disableFlip: false}, false);
+
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="scanner" width="600px">
+      
     </div>
   );
 }
